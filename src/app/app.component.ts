5c57eb617ab428e6ebc3f7cb2c0e81d0932af122
Wessel Steenkamp <66951387+wesse206@users.noreply.github.com>
2022-12-13 21:06:53 +0200

    Created imdb app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+
+export interface Movies {
+  results: any[]
+}
+
+@Injectable()
+export class MovieService {
+  constructor(private http: HttpClient) { }
+  apiUrl = 'https://imdb-api.com/API/AdvancedSearch/k_9qsnleq1/?title=Avengers'
+
+  getMovies() {
+    return this.http.get<Movies>(this.apiUrl)
+  }
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'imdb';
+export class AppComponent implements OnInit {
+  movies: Movies | undefined
+
+  constructor(private movieService: MovieService) { }
+
+  getMovies() {
+    this.movieService.getMovies()
+      .subscribe((data: Movies) => {
+        this.movies = {
+        results: data.results
+      }
+        console.log(JSON.stringify(this.movies))}
+      )
+    
+  }
+
+  ngOnInit() {
+    this.getMovies()
+    
+  }
+  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent, MovieService } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    MovieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
